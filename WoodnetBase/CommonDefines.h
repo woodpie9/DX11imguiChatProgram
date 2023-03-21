f7de41fee1f5c9bea43d900800b86688a562738e
woodpie9 <79443040+woodpie9@users.noreply.github.com>
2023-03-21 20:32:56 +0900

    ImGui를 사용한 클라이언트 UI 틀 잡기

---
+++ WoodnetBase/CommonDefines.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// NAMESPACE
+#define WOODNET_BEGIN	namespace woodnet {
+#define WOODNET_END		}
+
+#define DELEY_SERVER_ms 50
+#define MAX_NICKNAME_LEN 10
+#define MAX_CHATTING_LEN 30
+#define GAME_SER_KEY_LEN 20
+#define PROTOCOL_DECLARE struct
+
+typedef __int32 NetworkObjectID;
