e0fc349e126adc7ece418eaab0a05d4154346e67
woodpie9 <woodpie9@gmail.com>
2023-03-25 23:56:41 +0900

    프로젝트 이름 변경
    Client로 이름 변경

---
+++ ImGuiClient/ClientProgram.cpp
@@ -0,0 +1,595 @@
+
+#include "ClientProgram.h"
+#include "../WoodnetBase/WoodnetProtocol.h"
+
+
+
+static constexpr int PACKET_HEADER_SIZE = sizeof(PACKET_HEADER);
+
+ClientProgram::ClientProgram() : OnClient(false), OnConnect(false)
+{
+	set_last_error(_Error::success);
+}
+
+bool ClientProgram::init()
+{
+	m_log_msg.emplace_back("init client...");
+
+	// 이미 init 완료함
+	if (OnClient)
+	{
+		m_log_msg.emplace_back("already_init");
+		set_last_error(_Error::already_init);
+		return false;
+	}
+
+	// 이미 연결 되어있음
+	if (OnConnect)
+	{
+		m_log_msg.emplace_back("already_connect");
+		set_last_error(_Error::already_connect);
+		return false;
+	}
+
+	// 소켓을 연다.
+	if (m_Connector.Open(IPPROTO_TCP) == false)
+	{
+		m_log_msg.emplace_back("socket open fail");
+		set_last_error(_Error::fatal);
+		OnConnect = false;
+		return false;
+	}
+	set_connection_status(ConnectionStatus::Oppend);
+
+	// 소켓에 내가 관심있는 이벤트를 지정한다.
+	if (m_Connector.EventSelect(FD_CONNECT | FD_READ | FD_WRITE | FD_CLOSE) == false)
+	{
+		m_log_msg.emplace_back("eventselect fail");
+		set_last_error(_Error::fatal);
+		OnConnect = false;
+		return false;
+	}
+	set_connection_status(ConnectionStatus::SetEvent);
+
+
+	OnClient = true;
+
+	set_last_error(_Error::success);
+	return  true;
+}
+
+bool ClientProgram::connect_server(std::string ConnectIP)
+{
+	bool result = false;
+	m_log_msg.emplace_back("connecting server...");
+
+	// Init 필요
+	if (!OnClient)
+	{
+		m_log_msg.emplace_back("not_init");
+		set_last_error(_Error::not_init);
+		return false;
+	}
+
+	// 이미 연결 되어있음
+	if (OnConnect)
+	{
+		m_log_msg.emplace_back("already_connect");
+		set_last_error(_Error::already_connect);
+		return false;
+	}
+
+	// 소켓에 정보를 바인딩 한다.
+	SOCKADDR_IN ConnectAddr;
+	ConnectAddr.sin_family = AF_INET;
+	InetPtonA(AF_INET, ConnectIP.c_str(), &ConnectAddr.sin_addr);
+	ConnectAddr.sin_port = htons(m_port);
+
+	// 소켓의 정보로 서버에 연결한다.
+	if (m_Connector.Connect(ConnectAddr) == false)
+	{
+		m_log_msg.emplace_back("connect fail");
+		set_last_error(_Error::fatal);
+		OnConnect = false;
+		return false;
+	}
+
+	// 이벤트를 등록한다.
+	m_Event = m_Connector.GetEventHandle();
+
+	m_log_msg.push_back("Connect to " + ConnectIP + ":" + std::to_string(m_port));
+	set_connection_status(ConnectionStatus::Connecting);
+
+	OnConnect = true;
+	return true;
+}
+
+bool ClientProgram::login_server()
+{
+	if (check_connect() == false) return false;
+
+	m_log_msg.emplace_back("Login server...");
+
+	static MSG_C2S_ENTER_LOBBY C2SSendPacket;
+
+	C2SSendPacket.packet_size = sizeof(MSG_C2S_ENTER_LOBBY);
+	C2SSendPacket.packet_id = static_cast<INT16>(LOBBY_PACKET_ID::C2S_ENTER_LOBBY);
+	C2SSendPacket.seqNum = 1;
+
+	m_Connector.PostPacket(reinterpret_cast<char*>(&C2SSendPacket), C2SSendPacket.packet_size);
+
+	return true;
+}
+
+bool ClientProgram::set_nickname(std::string nickname)
+{
+	if (check_connect() == false) return false;
+
+	m_log_msg.emplace_back("Set Nickname...");
+
+	static MSG_C2S_ENTER_CHAT_SER C2SSendPacket;
+
+	C2SSendPacket.packet_size = sizeof(MSG_C2S_ENTER_CHAT_SER);
+	C2SSendPacket.packet_id = static_cast<INT16>(LOBBY_PACKET_ID::C2S_ENTER_CHAT_SER);
+	C2SSendPacket.seqNum = 1;
+
+	const char* cstr = nickname.c_str();
+	memcpy(C2SSendPacket.NickName, cstr, MAX_NICKNAME_LEN + 1);
+
+	m_Connector.PostPacket((char*)&C2SSendPacket, C2SSendPacket.packet_size);
+
+	return true;
+}
+
+bool ClientProgram::change_name(std::string nickname)
+{
+	if (check_connect() == false) return false;
+
+	m_log_msg.emplace_back("Change Nickname...");
+
+	static MSG_C2S_CHANGE_NAME C2SSendPacket;
+
+	C2SSendPacket.packet_size = sizeof(MSG_C2S_CHANGE_NAME);
+	C2SSendPacket.packet_id = static_cast<INT16>(LOBBY_PACKET_ID::C2S_CHANGE_NAME);
+	C2SSendPacket.seqNum = 1;
+
+	const char* cstr = nickname.c_str();
+	memcpy(C2SSendPacket.NickName, cstr, MAX_NICKNAME_LEN + 1);
+
+	m_Connector.PostPacket(reinterpret_cast<char*>(&C2SSendPacket), C2SSendPacket.packet_size);
+
+	return true;
+}
+
+bool ClientProgram::network_update()
+{
+	if (check_connect() == false) return false;
+
+	// 새로 들어온 이벤트가 있는지 확인한다.
+	static int EventReturn = 0;
+	if ((EventReturn = WSAWaitForMultipleEvents(1, &m_Event, FALSE, 1, FALSE)) == WSA_WAIT_FAILED)
+	{
+		m_log_msg.push_back("WSAWaitForMultipleEvents() failed with error " + std::to_string(WSAGetLastError()));
+		set_last_error(_Error::fatal);
+		return false;
+	}
+
+	if (EventReturn == WSA_WAIT_TIMEOUT)
+	{
+		// 타임아웃이면 통제권을 넘겨준다.
+		// SendFlush를 TimeOut때 할까?
+		send_flush();
+		set_last_error(_Error::success);
+		return true;
+	}
+
+	// 이번에 받은 이벤트가 어떤건지 확인한다.
+	WSANETWORKEVENTS NetworkEvents;
+	if (WSAEnumNetworkEvents(m_Connector.GetHandle(), m_Connector.GetEventHandle(), &NetworkEvents) == SOCKET_ERROR)
+	{
+		m_log_msg.push_back("WSAEnumNetworkEvents() failed with error " + std::to_string(WSAGetLastError()));
+		set_last_error(_Error::error);
+		return false;
+	}
+
+
+	if (NetworkEvents.lNetworkEvents & FD_CONNECT)
+	{
+		if (NetworkEvents.iErrorCode[FD_CONNECT_BIT] != 0)
+		{
+			m_log_msg.push_back("FD_CONNECT failed with error " + std::to_string(NetworkEvents.iErrorCode[FD_CONNECT_BIT]));
+			set_last_error(_Error::error);
+			return false;
+		}
+
+		on_net_connect(&m_Connector);
+	}
+
+
+	if (NetworkEvents.lNetworkEvents & FD_CLOSE)
+	{
+		if (NetworkEvents.iErrorCode[FD_CLOSE_BIT] != 0)
+		{
+			m_log_msg.push_back("FD_CONNECT failed with error " + std::to_string(NetworkEvents.iErrorCode[FD_CONNECT_BIT]));
+			set_last_error(_Error::error);
+			return false;
+		}
+
+		on_net_connect(&m_Connector);
+	}
+
+
+	if (NetworkEvents.lNetworkEvents & FD_READ)
+	{
+		if (NetworkEvents.iErrorCode[FD_READ_BIT] != 0)
+		{
+			m_log_msg.push_back("FD_CONNECT failed with error " + std::to_string(NetworkEvents.iErrorCode[FD_CONNECT_BIT]));
+			set_last_error(_Error::error);
+			return false;
+		}
+
+		on_net_recv(&m_Connector);
+	}
+
+
+	if (NetworkEvents.lNetworkEvents & FD_WRITE)
+	{
+		if (NetworkEvents.iErrorCode[FD_WRITE_BIT] != 0)
+		{
+			m_log_msg.push_back("FD_CONNECT failed with error " + std::to_string(NetworkEvents.iErrorCode[FD_CONNECT_BIT]));
+			set_last_error(_Error::error);
+			return false;
+		}
+
+		on_net_connect(&m_Connector);
+	}
+
+	return true;
+}
+
+//bool ClientProgram::clean_up()
+//{
+//	if (check_connect() == false) return false;
+//
+//	m_Connector.Close();
+//
+//	return false;
+//}
+
+bool ClientProgram::close()
+{
+	if (check_connect() == false) return false;
+
+	m_Connector.Close();
+
+	return true;
+}
+
+bool ClientProgram::send_chat_msg(std::string msg)
+{
+	if (check_connect() == false) return false;
+
+	static MSG_C2S_CHAT C2SSendPacket;
+
+	C2SSendPacket.packet_size = sizeof(MSG_C2S_CHAT);
+	C2SSendPacket.packet_id = static_cast<INT16>(LOBBY_PACKET_ID::C2S_CHAT);
+	C2SSendPacket.seqNum = 1;
+
+	const char* cMSG = msg.c_str();
+	memcpy(C2SSendPacket.MSG, cMSG, MAX_CHATTING_LEN + 1);
+
+	m_Connector.PostPacket(reinterpret_cast<char*>(&C2SSendPacket), C2SSendPacket.packet_size);
+
+
+	return true;
+}
+
+bool ClientProgram::check_connect()
+{
+	// Init 필요
+	if (!OnClient)
+	{
+		m_log_msg.emplace_back("not_init");
+		set_last_error(_Error::not_init);
+		return false;
+	}
+
+	// 이미 연결 되어있음
+	if (!OnConnect)
+	{
+		m_log_msg.emplace_back("not_connect");
+		set_last_error(_Error::not_connect);
+		return false;
+	}
+
+	return true;
+}
+
+bool ClientProgram::send(int PacketID, void* pPacket, int PacketLen)
+{
+	if (check_connect() == false) return false;
+
+	if (m_Connector.PostPacket(static_cast<char*>(pPacket), PacketLen) == false)
+	{
+		m_log_msg.emplace_back("PostPacket Fail");
+		set_last_error(_Error::error);
+		return false;
+	}
+
+	return true;
+}
+
+void ClientProgram::send_flush()
+{
+	m_Connector.SendUpdate();
+}
+
+
+
+// 네트워크 이벤트 관리
+bool ClientProgram::on_net_connect(woodnet::TCPSocket* pThisSocket)
+{
+	m_log_msg.emplace_back("Connect is OK!");
+	set_connection_status(ConnectionStatus::Connected);
+	set_last_error(_Error::success);
+	return true;
+}
+
+bool ClientProgram::on_net_close(woodnet::TCPSocket* pThisSocket)
+{
+	m_log_msg.emplace_back("close packet");
+	set_connection_status(ConnectionStatus::Closed);
+	set_last_error(_Error::success);
+	return true;
+}
+
+bool ClientProgram::on_net_recv(woodnet::TCPSocket* pThisSocket)
+{
+	static PACKET_HEADER msgHeader;
+	m_Connector.RecvUpdate();
+
+	while (m_Connector.PeekPacket(reinterpret_cast<char*>(&msgHeader), PACKET_HEADER_SIZE))
+	{
+		// 온전한 패킷이 있다면 패킷을 만들어서 처리.
+		memset(&m_PacketRecvBuf, 0, PACKET_SIZE_MAX);
+
+		if (msgHeader.packet_size > PACKET_SIZE_MAX || msgHeader.packet_size < 0)
+		{
+			m_log_msg.push_back("OnNetRecv packet size error " + std::to_string(msgHeader.packet_size));
+			set_last_error(_Error::error);
+			continue;
+		}
+
+		// 크기가 알맞는 하나의 패킷을 읽어온다.
+		if (m_Connector.ReadPacket(m_PacketRecvBuf, msgHeader.packet_size))
+		{
+			// 패킷을 해석하고 알맞는 동작을 한다.
+			PacketDispatcher(pThisSocket, m_PacketRecvBuf, msgHeader.packet_size);
+		}
+		else
+		{
+			m_log_msg.emplace_back("Connector.ReadPacket return false!");
+			break;
+		}
+	}
+
+	return true;
+}
+
+bool ClientProgram::on_net_send(woodnet::TCPSocket* pThisSocket)
+{
+	//TODO : 송신 가능한 상태 관리에 사용. 여기선 미사용
+	return true;
+}
+
+
+
+
+// 패킷 이벤트 관리
+bool ClientProgram::PacketDispatcher(woodnet::TCPSocket* RecvSock, void* pPacket, int len)
+{
+	PACKET_HEADER* pHeader = (PACKET_HEADER*)pPacket;
+
+	switch (pHeader->packet_id)
+	{
+		case static_cast<INT16>(COMMON_PAKET_ID::S2C_ACCEPT):
+		{
+			return on_packet_proc_accept(RecvSock, pPacket, len);
+		}
+		break;
+
+		case static_cast<INT16>(LOBBY_PACKET_ID::S2C_ENTER_LOBBY_OK):
+		{
+			return on_packet_proc_enter_lobby(RecvSock, pPacket, len);
+		}
+		break;
+
+		case static_cast<INT16>(LOBBY_PACKET_ID::S2C_ENTER_CHAT_SER_OK):
+		{
+			return on_packet_proc_enter_chat(RecvSock, pPacket, len);
+		}
+		break;
+
+		case static_cast<INT16>(LOBBY_PACKET_ID::S2C_NEW_PLAYER):
+		{
+			return on_packet_proc_new_player(RecvSock, pPacket, len);
+		}
+		break;
+
+		case static_cast<INT16>(LOBBY_PACKET_ID::S2C_LEAVE_PLAYER):
+		{
+			return on_packet_proc_leave_player(RecvSock, pPacket, len);
+		}
+		break;
+
+		case static_cast<INT16>(LOBBY_PACKET_ID::S2C_CHANGE_NAME_OK):
+		{
+			return on_packet_proc_change_name(RecvSock, pPacket, len);
+		}
+		break;
+
+		case static_cast<INT16>(LOBBY_PACKET_ID::S2C_CHAT):
+		{
+			return on_packet_proc_chat(RecvSock, pPacket, len);
+		}
+		break;
+
+		case static_cast<INT16>(LOBBY_PACKET_ID::S2C_NOTICE):
+		{
+			return on_packet_proc_notice(RecvSock, pPacket, len);
+		}
+		break;
+
+		default:
+		{
+			m_log_msg.push_back("Unknown Packet! id : " + std::to_string(pHeader->packet_id) + "size : " + std::to_string(pHeader->packet_size));
+			set_last_error(_Error::error);
+			return false;
+		}
+		break;
+	}
+}
+
+bool ClientProgram::on_packet_proc_accept(woodnet::TCPSocket* RecvSock, void* pS2CPacket, int C2SPacketLen)
+{
+	MSG_S2C_ACCEPT* S2CRcvPacket = static_cast<MSG_S2C_ACCEPT*>(pS2CPacket);
+
+	my_net_id_ = S2CRcvPacket->NetID;
+	m_log_msg.push_back("Enter Server. NetID : " + std::to_string(my_net_id_));
+
+	return true;
+}
+
+bool ClientProgram::on_packet_proc_enter_lobby(woodnet::TCPSocket* RecvSock, void* pS2CPacket, int C2SPacketLen)
+{
+	m_log_msg.emplace_back("pls set nickname");
+
+	return true;
+}
+
+bool ClientProgram::on_packet_proc_enter_chat(woodnet::TCPSocket* RecvSock, void* pS2CPacket, int C2SPacketLen)
+{
+	MSG_S2C_ENTER_CHAT_SER_OK* S2CRcvPacket = static_cast<MSG_S2C_ENTER_CHAT_SER_OK*>(pS2CPacket);
+
+	bool result = S2CRcvPacket->Result;
+
+	if (result == true)
+	{
+		m_log_msg.emplace_back("success Into Chat room!!!");
+		set_connection_status(ConnectionStatus::OnChat);
+		return false;
+	}
+	else
+	{
+		m_log_msg.emplace_back("fail into chat room...");
+
+		return true;
+	}
+}
+
+bool ClientProgram::on_packet_proc_new_player(woodnet::TCPSocket* RecvSock, void* pS2CPacket, int C2SPacketLen)
+{
+	MSG_S2C_NEW_PLAYER* S2CRcvPacket = static_cast<MSG_S2C_NEW_PLAYER*>(pS2CPacket);
+
+	std::string str = u8"새로운 클라이언트 접속. NetID : ";
+	INT32 id = S2CRcvPacket->NetID;
+	str += std::to_string(id);
+
+	str += " Name : ";
+	char Name[MAX_NICKNAME_LEN + 1];
+	memcpy(Name, S2CRcvPacket->NickName, MAX_NICKNAME_LEN + 1);
+	str += Name;
+
+	m_vector_msg.push_back(str);
+	m_log_msg.push_back(str);
+
+	return true;
+}
+
+bool ClientProgram::on_packet_proc_leave_player(woodnet::TCPSocket* RecvSock, void* pS2CPacket, int C2SPacketLen)
+{
+	MSG_S2C_LEAVE_PLAYER* S2CRcvPacket = static_cast<MSG_S2C_LEAVE_PLAYER*>(pS2CPacket);
+
+	std::string str = u8"클라이언트가 나갔습니다. NetID : ";
+	INT32 id = S2CRcvPacket->NetID;
+	str += std::to_string(id);
+
+	str += " Name : ";
+	char Name[MAX_NICKNAME_LEN + 1];
+	memcpy(Name, S2CRcvPacket->NickName, MAX_NICKNAME_LEN + 1);
+	str += Name;
+
+	m_vector_msg.push_back(str);
+	m_log_msg.push_back(str);
+
+	return true;
+}
+
+bool ClientProgram::on_packet_proc_change_name(woodnet::TCPSocket* RecvSock, void* pS2CPacket, int C2SPacketLen)
+{
+	MSG_S2C_CHANGE_NAME_OK* S2CRcvPacket = static_cast<MSG_S2C_CHANGE_NAME_OK*>(pS2CPacket);
+
+	std::string str = u8"클라이언트가 이름을 변경. NetID : ";
+	INT32 id = S2CRcvPacket->NetID;
+	str += std::to_string(id);
+
+	str += " Name : ";
+	char Name[MAX_NICKNAME_LEN + 1];
+	memcpy(Name, S2CRcvPacket->NickName, MAX_NICKNAME_LEN + 1);
+	str += Name;
+	
+	m_vector_msg.push_back(str);
+	m_log_msg.push_back(str);
+
+	return true;
+}
+
+bool ClientProgram::on_packet_proc_chat(woodnet::TCPSocket* RecvSock, void* pS2CPacket, int C2SPacketLen)
+{
+	MSG_S2C_CHAT* S2CRcvPacket = static_cast<MSG_S2C_CHAT*>(pS2CPacket);
+
+	std::string str = "NetID : ";
+	INT32 id = S2CRcvPacket->NetID;
+	str += std::to_string(id);
+
+	str += " Name : ";
+	char Name[MAX_NICKNAME_LEN + 1];
+	memcpy(Name, S2CRcvPacket->NickName, MAX_NICKNAME_LEN + 1);
+	str += Name;
+
+	str += " Chat : ";
+	char chat[MAX_CHATTING_LEN + 1];
+	memcpy(chat, S2CRcvPacket->MSG, MAX_CHATTING_LEN + 1);
+	str += chat;
+
+	m_vector_msg.push_back(str);
+	m_log_msg.push_back(str);
+
+	return true;
+}
+
+bool ClientProgram::on_packet_proc_notice(woodnet::TCPSocket* RecvSock, void* pS2CPacket, int C2SPacketLen)
+{
+	MSG_S2C_NOTICE* S2CRcvPacket = static_cast<MSG_S2C_NOTICE*>(pS2CPacket);
+
+	std::string str = "<<< 공지 !!! NetID : ";
+	INT32 id = S2CRcvPacket->NetID;
+	str += std::to_string(id);
+
+	str += " Name : ";
+	char Name[MAX_NICKNAME_LEN + 1];
+	memcpy(Name, S2CRcvPacket->NickName, MAX_NICKNAME_LEN + 1);
+	str += Name;
+
+	str += " Chat : ";
+	char chat[MAX_CHATTING_LEN + 1];
+	memcpy(chat, S2CRcvPacket->NickName, MAX_CHATTING_LEN + 1);
+	str += chat;
+
+	str += " >>>";
+	
+	m_vector_msg.push_back(str);
+	m_log_msg.push_back(str);
+
+	return true;
+}
---
+++ ImGuiClient/main.cpp
@@ -0,0 +1,263 @@
+// Dear ImGui: standalone example application for DirectX 11
+// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// Read online: https://github.com/ocornut/imgui/tree/master/docs
+
+
+// Client
+#include "../woodnetBase/WinNetwork.h"
+#include "ClientProgram.h"
+#include "dx11Imgui.h"
+
+woodnet::WinNetwork* network;
+ClientProgram* client;
+dx11Imgui* dx11_gui;
+
+
+static const char* connection_status_str[] = { " None",	"Oppend",	"SetEvent",	"Connecting",	"Connected",	"OnChat",	"Disconnected",	"Closed" };
+
+
+// Main code
+int main(int, char**)
+{
+	// Window State
+	bool show_demo_window = false;
+	bool show_chatting_client = true;
+	bool show_logger_window = true;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	network = new woodnet::WinNetwork();
+	client = new ClientProgram();
+	dx11_gui = new dx11Imgui();
+
+	// winsock2 사용 시작
+	network->Init();
+	client->init();
+	dx11_gui->init();
+
+	bool isConnect = false;
+	bool checkbox1 = false;
+	static char nickname[128] = "";
+	static char password[128] = "";
+	static char msgbox[128] = "";
+
+
+	// Main loop
+	bool done = false;
+	while (!done)
+	{
+		// Poll and handle messages (inputs, window resize, etc.)
+		// See the WndProc() function below for our to dispatch events to the Win32 backend.
+		MSG msg;
+		while (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+		{
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+			if (msg.message == WM_QUIT)
+				done = true;
+		}
+		if (done)
+			break;
+
+
+		// 화면을 갱신한다.
+		dx11_gui->newframe();
+
+
+		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+
+		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+			ImGui::SetNextWindowSize(ImVec2(400, 300), ImGuiCond_Always);
+			ImGui::SetNextWindowPos(ImVec2(30, 30));
+			ImGui::Begin(u8"Hello, world! 한글도 출력");             // Create a window called "Hello, world!" and append into it.
+
+			// 한글 출력
+			std::string stdstr = u8"한글한글";
+			ImGui::Text(stdstr.c_str());
+			ImGui::Text("This is some useful text.");					// Display some text (you can use a format strings too)
+			ImGui::Checkbox("Demo Window", &show_demo_window);			// Edit bools storing our window open/close state
+			ImGui::Checkbox(u8"체팅 클라이언트", &show_chatting_client);
+			ImGui::Checkbox("logger window", &show_logger_window);
+
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);	// Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", (float*)&clear_color);	// Edit 3 floats representing a color
+
+			if (ImGui::Button("Button"))								// Buttons return true when clicked (most widgets return true when edited/activated)
+				counter++;
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / dx11_gui->get_io().Framerate, dx11_gui->get_io().Framerate);
+			ImGui::End();
+		}
+
+
+		if (show_logger_window)
+		{
+			// log
+			ImGui::SetNextWindowPos(ImVec2(30, 350));
+			ImGui::SetNextWindowSize(ImVec2(300, 300), ImGuiCond_Always);
+			ImGui::Begin("logger");
+			{
+				ImGui::BeginChild("chatText", ImVec2(0, ImGui::GetFontSize() * 10.0f), true);
+
+				if(ImGui::BeginMenuBar())
+				{
+				ImGui::TextUnformatted(u8"Logger");
+				ImGui::EndMenuBar();
+				}
+
+				const int track_item = static_cast<int>(client->m_log_msg.size()) - 1;
+				for (int item = 0; item < client->m_log_msg.size(); item++)
+				{
+					if (item == track_item)
+					{
+						ImGui::TextColored(ImVec4(1, 1, 0, 1), client->m_log_msg[item].c_str());
+						ImGui::SetScrollHereY(1); // 0.0f:top, 0.5f:center, 1.0f:bottom
+					}
+					else
+					{
+						ImGui::Text(client->m_log_msg[item].c_str());
+					}
+				}
+
+				ImGui::EndChild();
+
+				ConnectionStatus connection = client->get_connection_status();
+				ImGui::Text(connection_status_str[(static_cast<int>(connection))]);
+			}
+			ImGui::End();
+		}
+
+
+		if (show_chatting_client)
+		{
+			if (isConnect == true)
+			{
+				// client network loop
+				client->network_update();
+			}
+
+			ImGui::SetNextWindowPos(ImVec2(600, 30));
+			ImGui::SetNextWindowSize(ImVec2(500, 600), ImGuiCond_Always);
+			ImGui::Begin("Chatting Client");
+			{
+				if (!isConnect)
+				{
+					const char* IPlist[] = { "127.0.0.1", "192.168.0.11", "192.168.0.33" };
+					static int IP_current = 0;
+					ImGui::Combo("IP", &IP_current, IPlist, IM_ARRAYSIZE(IPlist));
+					ImGui::SameLine();
+
+					if (ImGui::Button(u8"서버 접속"))
+					{
+						isConnect = true;
+						client->m_log_msg.push_back(const_cast<char*>(IPlist[IP_current]));
+						client->connect_server(IPlist[IP_current]);
+					}
+				}
+				else if (isConnect && client->get_connection_status() == ConnectionStatus::Connected)
+				{
+					ImGui::InputTextWithHint("Nickname", "enter text here", nickname, IM_ARRAYSIZE(nickname));
+
+					//ImGui::InputTextWithHint("password (w/ hint)", "<password>", password, IM_ARRAYSIZE(password), ImGuiInputTextFlags_Password);
+					//ImGui::Checkbox(u8"패드워드 확인", &checkbox1);
+					ImGui::SameLine();
+					if (ImGui::Button(u8"체팅방 입장"))
+					{
+						client->login_server();
+						client->m_log_msg.push_back(nickname);
+						client->m_log_msg.push_back(password);
+						client->set_nickname(nickname);
+					}
+
+					if (checkbox1)
+					{
+						ImGui::Text(password);
+					}
+
+					/*if (ImGui::Button(u8"접속 종료"))
+					{
+						isConnect = false;
+					}*/
+
+				}
+				else if (isConnect && client->get_connection_status() == ConnectionStatus::OnChat)
+				{
+					static int track_item;
+					static bool enable_track = true;
+					static bool enable_extra_decorations = false;
+
+					ImGui::Checkbox("Decoration", &enable_extra_decorations);
+					ImGui::SameLine();
+					ImGui::Checkbox("Track", &enable_track);
+					//ImGui::PushItemWidth(100);
+
+
+					ImGui::Text(u8"체팅 메시지 박스");
+
+
+					const ImGuiWindowFlags child_flags = enable_extra_decorations ? ImGuiWindowFlags_MenuBar : 0;
+					ImGui::BeginChild("chatText", ImVec2(0, ImGui::GetFontSize() * 20.0f), true, child_flags);
+					if (ImGui::BeginMenuBar())
+					{
+						ImGui::TextUnformatted(u8"체팅방 이름");
+						ImGui::EndMenuBar();
+					}
+					track_item = static_cast<int>(client->m_vector_msg.size()) - 1;
+					for (int item = 0; item < client->m_vector_msg.size(); item++)
+					{
+						if (enable_track && item == track_item)
+						{
+							ImGui::TextColored(ImVec4(1, 1, 0, 1), client->m_vector_msg[item].c_str());
+							ImGui::SetScrollHereY(1); // 0.0f:top, 0.5f:center, 1.0f:bottom
+						}
+						else
+						{
+							ImGui::Text(client->m_vector_msg[item].c_str());
+						}
+					}
+					ImGui::EndChild();
+
+
+					ImGui::InputTextWithHint(" ", "enter msg here", msgbox, IM_ARRAYSIZE(msgbox));
+					ImGui::SameLine();
+					if (ImGui::Button(u8"전송"))
+					{
+						client->send_chat_msg(msgbox);
+					}
+				}
+				else if( isConnect && client->get_connection_status()==ConnectionStatus::Connecting)
+				{
+					ImGui::Text(u8"연결중... 로그창 확인!!");
+				}
+				else
+				{
+					ImGui::Text(u8"여긴 누구 나는 어디... 로그창 확인!!");
+				}
+
+
+			}
+			ImGui::End();
+		}
+
+
+		if(!dx11_gui->render())
+		{
+			return 0;
+		}
+	}
+
+	// dx11, imgui 사용 종료
+	dx11_gui->cleanup();
+	// winsock2 사용 종료
+	network->CleanUp();
+
+	return 0;
+}
+
