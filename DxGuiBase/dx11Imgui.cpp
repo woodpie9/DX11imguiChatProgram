6106c4de7fe691748bb5284828d09ff1d0473223
woodpie9 <79443040+woodpie9@users.noreply.github.com>
2023-04-07 14:38:19 +0900

    주석 작성과 코드 정리

--- DxGuiBase/dx11Imgui.cpp
+++ DxGuiBase/dx11Imgui.cpp
@@ -1,6 +1,5 @@
 #include "DX11Imgui.h"
 
-
 // 전역 선언, 전방선언
 ID3D11Device* pd3dDevice = nullptr;
 ID3D11DeviceContext* pd3dDeviceContext = nullptr;
@@ -8,23 +7,16 @@ IDXGISwapChain* pSwapChain = nullptr;
 ID3D11RenderTargetView* mainRenderTargetView = nullptr;
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-// Helper functions
-DX11Imgui::DX11Imgui() //:pd3dDevice(nullptr), pd3dDeviceContext(nullptr), pSwapChain(nullptr), mainRenderTargetView(nullptr)
-{
-}
-
-DX11Imgui::~DX11Imgui()
-{
-}
-
 
 bool DX11Imgui::Init()
 {
 	// Create application window
 		//ImGui_ImplWin32_EnableDpiAwareness();
-	wc_ = { sizeof(wc_), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, L"ImGui Example", NULL };
+	wc_ = { sizeof(wc_), CS_CLASSDC, WndProc, 0L, 0L,
+		GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
+		L"ImGui Chatting", NULL };
 	::RegisterClassExW(&wc_);
-	hwnd_ = ::CreateWindowW(wc_.lpszClassName, L"Dear ImGui DirectX11 Example", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc_.hInstance, NULL);
+	hwnd_ = ::CreateWindowW(wc_.lpszClassName, L"Dear ImGui DirectX11 Chatting Program", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc_.hInstance, NULL);
 
 	// Initialize Direct3D
 	if (!CreateDeviceD3D(hwnd_))
@@ -54,25 +46,8 @@ bool DX11Imgui::Init()
 	ImGui_ImplDX11_Init(pd3dDevice, pd3dDeviceContext);
 
 	// Load Fonts 한글 폰트 추가
-	io.Fonts->AddFontFromFileTTF(R"(c:\Windows\Fonts\malgun.ttf)", 18.0f, NULL, io.Fonts->GetGlyphRangesKorean());
-
-	// Load Fonts
-	// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-	// - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-	// - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-	// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-	// - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-	// - Read 'docs/FONTS.md' for more instructions and details.
-	// - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-	//io.Fonts->AddFontDefault();
-	//io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-	//ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-	//IM_ASSERT(font != NULL);
-
-	
+	io.Fonts->AddFontFromFileTTF(R"(c:\Windows\Fonts\malgun.ttf)", 
+		18.0f, NULL, io.Fonts->GetGlyphRangesKorean());
 
 	return true;
 }
@@ -81,7 +56,8 @@ bool DX11Imgui::Render() const
 {
 	// Rendering
 	ImGui::Render();
-	const float clear_color_with_alpha[4] = { clear_color_.x * clear_color_.w, clear_color_.y * clear_color_.w, clear_color_.z * clear_color_.w, clear_color_.w };
+	const float clear_color_with_alpha[4]
+	= { clear_color_.x * clear_color_.w, clear_color_.y * clear_color_.w, clear_color_.z * clear_color_.w, clear_color_.w };
 	pd3dDeviceContext->OMSetRenderTargets(1, &mainRenderTargetView, NULL);
 	pd3dDeviceContext->ClearRenderTargetView(mainRenderTargetView, clear_color_with_alpha);
 	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
@@ -115,9 +91,6 @@ bool DX11Imgui::Cleanup()
 
 	return true;
 }
-
-
-
 
 
 bool DX11Imgui::CreateDeviceD3D(HWND hWnd)
--- DxGuiBase/dx11Imgui.h
+++ DxGuiBase/dx11Imgui.h
@@ -9,17 +9,17 @@
 
 //	20230407 코딩스타일 적용, 사용 주석 작성
 //	20230323 DX11 Imgui 분리 프로젝트 생성
-//
+//	
 class DX11Imgui
 {
 public:
-	DX11Imgui();
-	~DX11Imgui();
+	DX11Imgui() {};
+	~DX11Imgui() {};
 
-	bool Init();
-	bool Render() const;
-	bool Newframe();
-	bool Cleanup();
+	bool Init();			// 사용 전 가장 먼서 호출되야 함
+	bool Newframe();		// 매 프래임 시작하며 호출되어야 함
+	bool Render() const;	// 매 프레임 마지막에 호출되어야 함
+	bool Cleanup();			// 사용 종료 시 호출되어야 함
 
 	// 프레임 표시 등을 위한 Getter
 	ImGuiIO GetIo() { return ImGui::GetIO(); };
